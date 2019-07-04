2ee606227c10e6e837e5b2aac1d5f841fb426436
BrayanD07 <52518417+BrayanD07@users.noreply.github.com>
2019-07-04 12:02:18 -0500

    Add files via upload

---
+++ Persona.js
@@ -0,0 +1,65 @@
+﻿class Persona
+{
+constructor(n, e, c, al, an, x, y)
+{
+this.Nombre = n;
+this.Edad = e;
+this.Color = c;
+this.Altura = al;
+this.Ancho = an;
+this.X = x;
+this.Y = y;
+}
+
+Hablar(mensaje)
+{
+alert(mensaje);
+}
+
+Dibujar(contextoDeDibujo){
+
+//Crear una pluma con el color
+contextoDeDibujo.strokeStyle = this.Color; 
+
+//cabeza
+var radio = this.Ancho/2;
+contextoDeDibujo.beginPath();
+contextoDeDibujo.arc(this.X+radio,this.Y+radio,
+            	     radio,0,2*3.1416,true);
+contextoDeDibujo.stroke();
+
+//tronco
+contextoDeDibujo.beginPath();
+contextoDeDibujo.moveTo(this.X+radio,
+                        this.Y+2*radio);
+contextoDeDibujo.lineTo(this.X+radio,
+                        this.Y+3*radio);
+contextoDeDibujo.stroke();
+
+//manos
+contextoDeDibujo.moveTo(this.X,
+                        this.Y + 2.2*(radio));
+contextoDeDibujo.lineTo(this.X + 2*(radio),
+                        this.Y + 2.2*(radio));
+contextoDeDibujo.stroke();
+
+//piernaI
+contextoDeDibujo.moveTo(this.X + radio,
+                        this.Y + 3*(radio));
+contextoDeDibujo.lineTo(this.X + 2*(radio),
+                        this.Y + 4*(radio));
+contextoDeDibujo.stroke();
+
+//piernaD
+contextoDeDibujo.moveTo(this.X + radio,
+                        this.Y + 3*(radio));
+contextoDeDibujo.lineTo(this.X ,
+                        this.Y + 4*(radio));
+contextoDeDibujo.stroke();
+
+//Nombre
+contextoDeDibujo.font="bold italic 12px arial";
+contextoDeDibujo.fillText(this.Nombre,this.X,this.Y-20)
+
+}
+}
---
+++ main.js
@@ -0,0 +1,54 @@
+﻿function crearPersona()
+{
+var myCanvas = document.getElementById("myCanvas");
+var context = myCanvas.getContext("2d");
+
+var arregloPersonas = new Array(new Persona("hector",15,"black",108,70,10,180),
+			       new Persona("Iris",21,"blue",80,45,80,180),
+			       new Persona("juan",17,"green",95,30,150,180),
+			       new Persona("pablo",19,"orange",120,55,220,180),
+			       new Persona("pedro",12,"red",100,50,300,180));
+
+for (var u = arregloPersonas.length - 1; u >= 0; u--) 
+{
+arregloPersonas[u].Dibujar(context);
+}
+}
+
+
+/*
+function crearArbol()
+{
+
+var myCanvas = document.getElementById("myCanvas");
+var context = myCanvas.getContext("2d");
+
+//Declaración, Creación e Inicialización de un arreglo
+var arregloArboles = new Array(new Arbol("blue", 100, 80, 10, 350, 5, 1),
+			       new Arbol("black", 82, 50, 120, 350, 5, 1),
+			       new Arbol("red",70, 70, 200, 350, 5, 1),
+			       new Arbol("orange", 97, 67, 300, 350, 5, 1),
+			       new Arbol("green", 80, 66, 400, 350, 5, 1));
+for (var o = arregloArboles.length - 1; o >= 0; o--) 
+{
+arregloArboles[o].Dibujar(context);
+}
+}
+
+
+function crearCubo()
+{
+var myCanvas = document.getElementById("myCanvas");
+var context = myCanvas.getContext("2d");
+
+var arregloCubos = new Array(new Cubo("red",30,30,850,27000,10,50),
+			       new Cubo("green",40,40,40,64000,100,50),
+			       new Cubo("blue",20,20,20,8000,190,50),
+			       new Cubo("orange",35,35,35,42875,280,50),
+			       new Cubo("black",25,25,25,15625,360,50));
+for (var p = arregloCubos.length - 1; p >= 0; p--) 
+{
+arregloCubos[p].Dibujar(context);
+}
+}
+*/
